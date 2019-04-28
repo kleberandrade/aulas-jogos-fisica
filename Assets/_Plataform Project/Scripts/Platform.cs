a74c957587d4750351d799cd53d6adfaa0f3617f
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2019-04-27 22:40:40 -0300

    Projeto de plataformas móveis

--- Assets/_Plataform Project/Scripts/Platform.cs
+++ Assets/_Plataform Project/Scripts/Platform.cs
@@ -3,46 +3,53 @@
 public class Platform : MonoBehaviour
 {
     [SerializeField]
-    private Transform[] m_WayPoints;
+    private Transform[] m_Points;
 
     [SerializeField]
     private float m_Speed;
 
     [SerializeField]
-    private float m_Radius = 1;
+    private float m_Radius = 0.01f;
 
     [SerializeField]
-    private float m_TimeToNextWayPoint;
+    private float m_TimeToNextPoint;
 
     private bool m_Waiting;
 
-    private int m_Index = 0;
+    private int m_Index;
 
-    private int m_Step = 1;
+    private int m_Direction = 1;
+
+    public bool IsEmpty => m_Points.Length == 0;
 
     public void Update()
     {
+        if (IsEmpty)
+        {
+            return;
+        }
+
         if (!m_Waiting)
         {
-            if (Vector3.Distance(m_WayPoints[m_Index].position, transform.position) <= m_Radius)
+            if (Vector3.Distance(m_Points[m_Index].position, transform.position) <= m_Radius)
             {
                 m_Waiting = true;
-                transform.position = m_WayPoints[m_Index].transform.position;
-                Invoke("NextWayPoint", m_TimeToNextWayPoint);
+                transform.position = m_Points[m_Index].transform.position;
+                Invoke("NextWayPoint", m_TimeToNextPoint);
             }
 
-            transform.position = Vector3.MoveTowards(transform.position, m_WayPoints[m_Index].position, Time.deltaTime * m_Speed);
+            transform.position = Vector3.MoveTowards(transform.position, m_Points[m_Index].position, Time.deltaTime * m_Speed);
         }
     }
 
     private void NextWayPoint()
     {
-        m_Index += m_Step;
+        m_Index += m_Direction;
 
-        if (m_Index >= m_WayPoints.Length || m_Index < 0)
+        if (m_Index >= m_Points.Length || m_Index < 0)
         {
-            m_Step *= -1;
-            m_Index += m_Step;
+            m_Direction *= -1;
+            m_Index += m_Direction * 2;
         }
 
         m_Waiting = false;
