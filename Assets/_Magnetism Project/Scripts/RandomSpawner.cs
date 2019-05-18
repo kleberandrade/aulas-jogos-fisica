38649a8299d88537bf9f5dc217932f38bf58b751
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2019-05-18 11:55:01 -0300

    Update Battle Car

--- Assets/_Magnetism Project/Scripts/RandomSpawner.cs
+++ Assets/_Magnetism Project/Scripts/RandomSpawner.cs
@@ -11,13 +11,20 @@ public class RandomSpawner : MonoBehaviour
     [SerializeField]
     private float m_Range;
 
+    [SerializeField]
+    private float m_MinObjectScale;
+
+    [SerializeField]
+    private float m_MaxObjectScale;
+
     private void Start()
     {
         for (int i = 0; i < m_ObjectsNumber; i++)
         {
             Vector3 position = Random.insideUnitSphere * m_Range;
             Quaternion rotation = Quaternion.Euler(Random.insideUnitSphere * 360.0f);
-            Instantiate(m_ObjectPrefab, position, rotation);
+            GameObject go = Instantiate(m_ObjectPrefab, position, rotation);
+            go.transform.localScale = Vector3.one * Random.Range(m_MinObjectScale, m_MaxObjectScale);
         }        
     }
 }
